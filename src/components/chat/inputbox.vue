7ea6f6768307c986916bfbe8d95b24b12224e25e
123summertime <2361011391@qq.com>
2024-01-16 03:54:18 +0800

    上传文件按钮

--- src/components/chat/inputbox.vue
+++ src/components/chat/inputbox.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="inputBoxRoot">
     <div class="bar">
-      <el-button type="primary" @click="sending('text', message)" :disabled="message ? false : true"
-        class="send">发送</el-button>
+      <div>
+        <label class="fileLabel" for="fileUpload">
+          <FolderAdd class="icon" />
+        </label>
+        <input type="file" id="fileUpload" @change="fileUpload">
+      </div>
+      <el-button class="send" type="primary" @click="sending('text', message)"
+        :disabled="message ? false : true">发送</el-button>
     </div>
     <textarea v-model=message @keydown="onKeyDown" v-on:paste="pasteImg"></textarea>
 
@@ -11,7 +17,7 @@
       <template #footer>
         <span class="footer">
           <div class="imgInfo">
-            <div class="msgName">{{ msgName }}</div> 
+            <div class="msgName">{{ msgName }}</div>
             <div class="msgSize">{{ msgSize + "KB" }}</div>
           </div>
           <div class="buttons">
@@ -63,17 +69,45 @@ export default {
       }
     },
 
-    beforeSending() {
-      this.visible = true
+    fileUpload(event) {
+      const file = event.target.files[0]
+
+      if (file) {
+        this.msgName = file.name
+        const reader = new FileReader()
+        
+        reader.onload = () => {
+          const base64 = reader.result
+          const MIME = file.type.toLowerCase().split('/')[0]
+          if (MIME === "image") {
+            this.toWebpBase64(base64)
+          }
+        }
+        reader.readAsDataURL(file)
+      }
     },
 
-    confirmed() {
-      this.visible = false
-      this.sending("image", this.msgPayload)
-    },
+    toWebpBase64(base64) {
+      let img = new Image();
+      img.src = base64;
 
-    canceled() {
-      this.visible = false
+      img.onload = () => {
+        let canvas = document.createElement('canvas')
+        let ctx = canvas.getContext('2d')
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0)
+
+        canvas.toBlob((webpBlob) => {
+          this.msgSize = (webpBlob.size / 1000).toFixed(2)
+          let readerWebP = new FileReader()
+          readerWebP.onload = (eventWebP) => {
+            this.msgPayload = eventWebP.target.result
+            this.beforeSending()
+          }
+          readerWebP.readAsDataURL(webpBlob)
+        }, 'image/webp')
+      }
     },
 
     pasteImg(event) {
@@ -86,32 +120,26 @@ export default {
           this.msgName = blob.name
 
           reader.onload = (event) => {
-            let img = new Image()
-            img.src = event.target.result
-
-            img.onload = () => {
-              let canvas = document.createElement('canvas')
-              let ctx = canvas.getContext('2d')
-              canvas.width = img.width
-              canvas.height = img.height
-              ctx.drawImage(img, 0, 0)
-
-              canvas.toBlob((webpBlob) => {
-                this.msgSize = (webpBlob.size / 1000).toFixed(2)
-                let readerWebP = new FileReader()
-                readerWebP.onload = (eventWebP) => {
-                  this.msgPayload = eventWebP.target.result
-                  this.beforeSending()
-                }
-                readerWebP.readAsDataURL(webpBlob)
-              }, 'image/webp')
-            }
+            this.toWebpBase64(event.target.result)
           }
 
           reader.readAsDataURL(blob)
         }
       }
-    }
+    },
+
+    beforeSending() {
+      this.visible = true
+    },
+
+    confirmed() {
+      this.visible = false
+      this.sending("image", this.msgPayload)
+    },
+
+    canceled() {
+      this.visible = false
+    },
   }
 }
 </script>
@@ -124,9 +152,29 @@ export default {
 }
 
 .bar {
+  display: flex;
   width: 100%;
-  height: 36px;
+  height: 32px;
   background-color: brown;
+}
+
+input[type="file"] {
+  display: none;
+}
+
+.fileLabel {
+  display: block;
+  width: 64px;
+  height: 100%;
+  cursor: pointer;
+  background-color: #3498db;
+  color: black;
+}
+
+.icon {
+  display: block;
+  height: 100%;
+  margin: 0 auto;
 }
 
 textarea {
@@ -145,6 +193,7 @@ textarea::-webkit-scrollbar {
   display: none;
 }
 
+/* mask */
 .previewImg {
   display: block;
   max-width: 100%;
